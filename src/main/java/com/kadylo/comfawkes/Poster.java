5c7ac63d86c417b90140858b66da3922fc8210f0
HolyKadylo <pivas47@gmail.com>
2017-07-09 22:42:25 +0300

    cssSelector fix-2

--- src/main/java/com/kadylo/comfawkes/Poster.java
+++ src/main/java/com/kadylo/comfawkes/Poster.java
@@ -162,7 +162,7 @@ public class Poster extends Node{
 				System.out.println("-->8");
 				sleep(1000); 
 				System.out.println("-->9");
-				element = driver.findElement(By.cssSelector("a.ms_item.ms_item_photo._type_photo]"));
+				element = driver.findElement(By.cssSelector("a._type_photo]"));
 				//element = driver.findElement(By.className("ms_item ms_item_photo _type_photo"));
 				System.out.println("-->10");
 				element.click();
