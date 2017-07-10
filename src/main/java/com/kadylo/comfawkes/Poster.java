08c668d7386f81aa8d6e42a41a5bd9186e140004
HolyKadylo <pivas47@gmail.com>
2017-07-10 21:43:29 +0300

    using xpath in finding photo item

--- src/main/java/com/kadylo/comfawkes/Poster.java
+++ src/main/java/com/kadylo/comfawkes/Poster.java
@@ -162,8 +162,9 @@ public class Poster extends Node{
 				System.out.println("-->8");
 				sleep(1000); 
 				System.out.println("-->9");
-				element = driver.findElement(By.cssSelector("a._type_photo"));
+				//element = driver.findElement(By.cssSelector("a.ms_item.ms_item_photo_type_photo"));
 				//element = driver.findElement(By.className("ms_item ms_item_photo _type_photo"));
+				element = driver.findElement(By.xpath("//a[@class='ms_item ms_item_photo _type_photo']"));
 				System.out.println("-->10");
 				element.click();
 				System.out.println("-->11");
