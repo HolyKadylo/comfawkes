10fc7e2dc8b3c6b818506fc7c18856e2e1a44863
HolyKadylo <pivas47@gmail.com>
2017-08-05 21:26:29 +0300

    using extended classes

--- src/main/java/com/kadylo/comfawkes/Listener.java
+++ src/main/java/com/kadylo/comfawkes/Listener.java
@@ -189,7 +189,7 @@ public class Listener extends Node{
 					//WebElement unreadBar = driver.findElement(By.cssSelector("h4.im-page--history-new-bar._im_unread_bar_row"));
 					//...........final WebElement errorElement = web.findElement(By.xpath("//*[@id='" + elementId + "']/following-sibling::span[@class='error']"));
 					//ArrayList<WebElement> elements = new ArrayList(driver.findElements(By.cssSelector("div.im-mess-stack._im_mess_stack")));
-					element = driver.findElement(By.xpath("//h4[@class='im-page--history-new-bar']/following-sibling::div[@class='im-mess-stack']"));
+					element = driver.findElement(By.xpath("//h4[@class='im-page--history-new-bar _im_unread_bar_row']/following-sibling::div[@class='im-mess-stack _im_mess_stack']"));
 					/* try{
 						element = elements.get(elements.size() - 1);
 					} catch (IndexOutOfBoundsException ioobe){
