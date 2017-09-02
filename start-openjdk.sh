dfcb37198a054500d6bdda6c2e485b5b97c0e8d7
HolyKadylo <pivas47@gmail.com>
2017-09-02 21:14:52 +0300

    unrun + target/

--- start-openjdk.sh
+++ start-openjdk.sh
@@ -3,5 +3,5 @@
 # $2 args
 
 echo "-->starting java app"
-sudo docker run -d -h "$1" --name "$1" -v "$PWD":/usr/src/myapp -w /usr/src/myapp openjdk java -jar comfawkes-1.0-SNAPSHOT-jar-with-dependencies.jar "$2 $3 $4 $5 $6 $7 $8 $9"
+sudo docker run -d -h "$1" --name "$1" -v "$PWD":/usr/src/myapp -w /usr/src/myapp openjdk java -jar target/comfawkes-1.0-SNAPSHOT-jar-with-dependencies.jar "$2 $3 $4 $5 $6 $7 $8 $9"
 echo "-->java app done"
---
+++ unrun.bash
@@ -0,0 +1,30 @@
+# This script stops as many Docker containers
+# as specified in $1
+
+# will be conctated with order number _X
+seleniumNodeName="node_"
+
+# will be conctated with order number _X
+applicationNodeName="app_"
+
+# we need only one RMQ per JVM TODO find out
+RMQServerName="appRabbit"
+
+echo "-->Stopping RMQServer"
+./stop-container.sh "$RMQServerName"
+
+echo "-->Stopping apps and nodes"
+nodesCount=0
+i=0
+for ((i=0; i<=$j; i++)); do
+	
+	# launching selenium node
+	./stop-container.sh "$seleniumNodeName""$nodesCount"
+	
+	# launching application
+	./stop-container.sh "$applicationNodeName""$nodesCount"
+	((nodesCount++))
+done
+echo "-->done unrun.sh"
+
+exit 0
