475a8136e63c3ef7292ed0e31d6017375f0f0607
HolyKadylo <pivas47@gmail.com>
2017-09-02 22:30:26 +0300

    fix passing params

--- start-openjdk.sh
+++ start-openjdk.sh
@@ -3,5 +3,5 @@
 # $2 args
 
 echo "-->starting java app with params $1 $2 $3 $4 $5 $6 $7 $8 $9"
-sudo docker run -d -h "$1" --name "$1" -v "$PWD":/usr/src/myapp -w /usr/src/myapp openjdk java -jar target/comfawkes-1.0-SNAPSHOT-jar-with-dependencies.jar $2 $3 $4 $5 $6 $7 $8 $9
+sudo docker run -d -h "$1" --name "$1" -v "$PWD":/usr/src/myapp -w /usr/src/myapp openjdk java -jar target/comfawkes-1.0-SNAPSHOT-jar-with-dependencies.jar $2 $3 $4 $5 $6 $7 $8 $9 $10
 echo "-->java app done"
