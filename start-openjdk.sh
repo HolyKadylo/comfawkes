bbf5d56f87ceac7ca039521e4482860f797abb28
HolyKadylo <pivas47@gmail.com>
2017-09-02 21:48:48 +0300

    logs & params

--- start-openjdk.sh
+++ start-openjdk.sh
@@ -2,6 +2,6 @@
 # $1 name
 # $2 args
 
-echo "-->starting java app"
-sudo docker run -d -h "$1" --name "$1" -v "$PWD":/usr/src/myapp -w /usr/src/myapp openjdk java -jar target/comfawkes-1.0-SNAPSHOT-jar-with-dependencies.jar "$2 $3 $4 $5 $6 $7 $8 $9"
+echo "-->starting java app with params $1 $2 $3 $4 $5 $6 $7 $8 $9"
+sudo docker run -d -h "$1" --name "$1" -v "$PWD":/usr/src/myapp -w /usr/src/myapp openjdk java -jar target/comfawkes-1.0-SNAPSHOT-jar-with-dependencies.jar $2 $3 $4 $5 $6 $7 $8 $9
 echo "-->java app done"
